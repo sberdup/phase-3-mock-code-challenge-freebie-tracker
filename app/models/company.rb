49fa1be983eceb9e8cf1a5d38d1064c26160b4b6
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-02 17:39:33 -0600

    Completed assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,11 @@
 class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
+    def give_freebie(dev, item_name, value)
+        #shoveling it into the dev's associated freebies
+        dev.freebies << Freebie.create(item_name: item_name, value:value, company_id: self.id)
+    end
+    def self.oldest_company
+        self.order(:founding_year).first
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,14 @@
 class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
+    def received_one?(item_name)
+        self.freebies.any?{|freebie| freebie.item_name == item_name}
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.dev_id = dev.id
+        end
+        #a separate line utilizing .save is needed to make this change in the DB
+    end
 end
